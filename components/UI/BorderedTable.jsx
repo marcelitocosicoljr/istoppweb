d2f9e695532ce1c8ef9fc17c2433438b4d62701f
Marcelito Cosicol <mcosicoljr@gmail.com>
2022-11-12 00:20:41 +0800

    finished 6 ui components with different variant. but still need to fix some.

--- components/UI/BorderedTable.jsx
+++ components/UI/BorderedTable.jsx
@@ -7,64 +7,72 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Label from './Label';
+import InputField from './InputField';
 
 
 
 const dataArray = [
   {
-    'franchiseNumber': '202902370',
+    'plateNumber': '77109393898',
     "operatorName" : 'Michael Dela Cruz',
-    'tricycleNumber': '77109393898',
-    "tricycleColor" : 'White',
-    'dateMonitored': 'October 3, 2022',
+    'franchiseNumber': '202902370',
+    "puvType" : 'White',
+    'lastUpdate': 'October 3, 2022',
+    "route" : 'Active',
     "status" : 'Active',
   },
   {
+    'plateNumber': '77109393898',
+    "operatorName" : 'Jessie Aves',
     'franchiseNumber': '202902370',
-    "operatorName" : 'Jessa Aves',
-    'tricycleNumber': '77109393898',
-    "tricycleColor" : 'White',
-    'dateMonitored': 'October 3, 2022',
+    "puvType" : 'White',
+    'lastUpdate': 'October 3, 2022',
+    "route" : 'Active',
     "status" : 'Active',
   },
   {
+    'plateNumber': '77109393898',
+    "operatorName" : 'Mark Ley',
     'franchiseNumber': '202902370',
-    "operatorName" : 'Norman Lido',
-    'tricycleNumber': '77109393898',
-    "tricycleColor" : 'White',
-    'dateMonitored': 'October 3, 2022',
+    "puvType" : 'White',
+    'lastUpdate': 'October 3, 2022',
+    "route" : 'Active',
     "status" : 'Active',
   },
   {
+    'plateNumber': '77109393898',
+    "operatorName" : 'Nion Martin',
     'franchiseNumber': '202902370',
-    "operatorName" : 'Martin Nights',
-    'tricycleNumber': '77109393898',
-    "tricycleColor" : 'White',
-    'dateMonitored': 'October 3, 2022',
+    "puvType" : 'White',
+    'lastUpdate': 'October 3, 2022',
+    "route" : 'Active',
     "status" : 'Active',
   },
   {
+    'plateNumber': '77109393898',
+    "operatorName" : 'Sandro Zer',
     'franchiseNumber': '202902370',
-    "operatorName" : 'Mark After',
-    'tricycleNumber': '77109393898',
-    "tricycleColor" : 'White',
-    'dateMonitored': 'October 3, 2022',
+    "puvType" : 'White',
+    'lastUpdate': 'October 3, 2022',
+    "route" : 'Active',
     "status" : 'Active',
   },
   {
+    'plateNumber': '77109393898',
+    "operatorName" : 'Angelo Baycee',
     'franchiseNumber': '202902370',
-    "operatorName" : 'Garyl Ong',
-    'tricycleNumber': '77109393898',
-    "tricycleColor" : 'White',
-    'dateMonitored': 'October 3, 2022',
+    "puvType" : 'White',
+    'lastUpdate': 'October 3, 2022',
+    "route" : 'Active',
     "status" : 'Active',
   },
   {
+    'plateNumber': '77109393898',
+    "operatorName" : 'Melchor Rodrigal',
     'franchiseNumber': '202902370',
-    "operatorName" : 'Nestor White',
-    'tricycleNumber': '77109393898',
-    "tricycleColor" : 'White',
-    'dateMonitored': 'October 3, 2022',
+    "puvType" : 'White',
+    'lastUpdate': 'October 3, 2022',
+    "route" : 'Active',
     "status" : 'Active',
   },
 ];
@@ -74,40 +82,56 @@ export default function BorderedTable() {
     <TableContainer  sx={{bgcolor:'transparent'}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead >
-          <TableRow  sx={{height: '130px'}}>
+          <TableRow  sx={{height: '15px', bgcolor:'#030744'}}>
             <TableCell>
-                <Label text='Francise Number' color='blue'/>
+                <Label text='Plate Number' color='blue'/>
             </TableCell>
             <TableCell >
                 <Label text="Operator's Name" color='blue'/>
             </TableCell>
             <TableCell>
-                <Label text='Tricycle Number' color='blue'/>
+                <Label text='Franchise Number' color='blue'/>
             </TableCell>
             <TableCell >
-                <Label text='Tricycle Color' color='blue'/>
+                <Label text='PUV Type' color='blue'/>
             </TableCell>
             <TableCell>
-                <Label text='Date Monitored' color='blue'/>
+                <Label text='Last Update' color='blue'/>
+            </TableCell>
+            <TableCell >
+                <Label text='Route' color='blue'/>
             </TableCell>
             <TableCell >
                 <Label text='Status' color='blue'/>
             </TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {dataArray.map((row) => (
+        <TableBody sx={{ 'tr:nth-child(even)': { background: '#DEDCDC' },'tr:nth-child(odd)': { background: '#fff' } }}>
+
+          <TableRow className='tableSearch'>
+
+              <TableCell sx={{maxWidth:'220px'}}><InputField placeHolder='Search Plate Number'/></TableCell>
+              <TableCell ><InputField placeHolder='Search Name'/></TableCell>
+              <TableCell ><InputField placeHolder='Search Franchise Number'/></TableCell>
+              <TableCell sx={{maxWidth:'190px'}}><InputField placeHolder='Search PUV Type'/></TableCell>
+              <TableCell ><InputField placeHolder='Search Date'/></TableCell>
+              <TableCell sx={{maxWidth:'160px'}} ><InputField placeHolder='Search Route'/></TableCell>
+              <TableCell sx={{maxWidth:'160px'}}><InputField placeHolder='Search Status'/></TableCell>
+
+          </TableRow>
+
+          {dataArray.map((row,index) => (
             <TableRow
-              key={row.franchiseNumber}
-               sx={{ 'td': { border: '1.5px solid #030744' } }}
+              key={index}
             >
               <TableCell  scope="row" >
-                {row.franchiseNumber}
+                {row.plateNumber}
               </TableCell>
               <TableCell >{row.operatorName}</TableCell>
-              <TableCell >{row.tricycleNumber}</TableCell>
-              <TableCell >{row.tricycleColor}</TableCell>
-              <TableCell >{row.dateMonitored}</TableCell>
+              <TableCell >{row.franchiseNumber}</TableCell>
+              <TableCell >{row.puvType}</TableCell>
+              <TableCell >{row.lastUpdate}</TableCell>
+              <TableCell >{row.route}</TableCell>
               <TableCell >{row.status}</TableCell>
             </TableRow>
           ))}
---
+++ pages/uicomponents.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const uicomponents = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default uicomponents
